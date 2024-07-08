9096bdee465134d1ddcd234af98073218a2f67a1
ivxmirov <ivxmirov@gmail.com>
2024-07-08 23:59:30 +0400

    docstrings are ready

--- homework.py
+++ homework.py
@@ -1,11 +1,14 @@
-...
+import os
+from datetime import time
+from dotenv import load_dotenv
+from telebot import TeleBot
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,22 +23,59 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
+    """
+    Проверь доступность переменных окружения.
+
+    Проверяет доступность переменных окружения, которые необходимы для работы
+    программы. Если отсутствует хотя бы одна переменная окружения — продолжать
+    работу бота нет смысла.
+    """
     ...
 
 
 def send_message(bot, message):
+    """
+    Отправь сообщение.
+
+    Отправляет сообщение в Telegram-чат, определяемый переменной окружения
+    TELEGRAM_CHAT_ID. Принимает на вход два параметра: экземпляр класса
+    TeleBot и строку с текстом сообщения.
+    """
     ...
 
 
 def get_api_answer(timestamp):
+    """
+    Получи ответ API.
+
+    Делает запрос к единственному эндпоинту API-сервиса. В качестве параметра
+    в функцию передаётся временная метка. В случае успешного запроса должна
+    вернуть ответ API, приведя его из формата JSON к типам данных Python.
+    """
     ...
 
 
 def check_response(response):
+    """
+    Проверь ответ API.
+
+    Проверяет ответ API на соответствие документации из урока «API сервиса
+    Практикум Домашка». В качестве параметра функция получает ответ API,
+    приведённый к типам данных Python.
+    """
     ...
 
 
 def parse_status(homework):
+    """
+    Извлеки статус.
+
+    Извлекает из информации о конкретной домашней работе статус этой работы.
+    В качестве параметра функция получает только один элемент из списка
+    домашних работ. В случае успеха функция возвращает подготовленную для
+    отправки в Telegram строку, содержащую один из вердиктов словаря
+    HOMEWORK_VERDICTS.
+    """
     ...
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
@@ -43,14 +83,13 @@ def parse_status(homework):
 
 def main():
     """Основная логика работы бота."""
-
     ...
 
     # Создаем объект класса бота
     bot = ...
     timestamp = int(time.time())
 
-    ...
+    time.sleep(RETRY_PERIOD)
 
     while True:
         try:
